9f4891549f48098bffd4d8bc878890aaede3331a
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-05-03 17:11:16 +0100

    chore: improved script

--- scripts/upload-app.sh
+++ scripts/upload-app.sh
@@ -6,17 +6,21 @@ git config --global user.email "adityaofficialgupta@gmail.com"
 git clone https://adityastic:$GITHUB_KEY@github.com/adityastic/AndroidAppReleases
 cd AndroidAppReleases
 
-rm -rf rans*
 rm -rf ${TRAVIS_REPO_SLUG#*/}*
 
 find ../app/build/outputs -type f -name '*.apk' -exec cp -v {} . \;
 
+git checkout --orphan temporary
+
 for file in app*; do
     if [[ $file != *"unsigned"* ]];then
-        mv $file ${TRAVIS_REPO_SLUG#*/}-${file:4}
-        git add ${TRAVIS_REPO_SLUG#*/}-${file:4}
-        git commit -am "Update ${TRAVIS_REPO_SLUG#*/}-${file:4} ($(date +%Y-%m-%d.%H:%M:%S))"
+        mv $file ${TRAVIS_REPO_SLUG#*/}-${file#*-}
+        git add ${TRAVIS_REPO_SLUG#*/}-${file#*-}
+        git commit -am "Update ${TRAVIS_REPO_SLUG#*/}-${file#*-} ($(date +%Y-%m-%d.%H:%M:%S))"
     fi
 done
 
-git push origin master
+git branch -D master
+git branch -m master
+
+git push origin master --force
